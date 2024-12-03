83d2acd1a2ef481065ed26b60dc783b1d172d0af
Shawn Weed <shawn.weed@icloud.com>
2024-12-03 14:47:35 -0500

    Added panda dataframes
    Added functionality to print the serial number for devices assigned to each student in a specific homeroom by joining on email.

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from zebra import Zebra
 from label import Label
+import pandas as pd
+from dotenv import load_dotenv
+import os
 
-z = Zebra()
+load_dotenv()
 
-label = Label("Test")
+z = Zebra('ZDesigner ZD411-203dpi ZPL')
 
-z.send_command(label.create_label())
+hr_file = os.environ.get("HOMEROOM_FILE")
+device_file = os.environ.get("DEVICE_FILE")
+
+hr = pd.read_excel(hr_file, usecols='C')
+devices = pd.read_csv(device_file, usecols=[2,7])
+
+df = pd.merge(left = devices, right = hr, how='right', left_on='Student (email)', right_on='Email')
+
+for index, row in df.iterrows():
+    label = Label(f"{row['Serial Number']}")
+    z.output(label.zebra_image)
--- label.py
+++ label.py
@@ -6,7 +6,6 @@ class Label:
     def __init__(self, serial_number):
         self.serial_number = serial_number
 
-    def create_label(self):
         #Create qrcode
         qr = qrcode.make(f'{self.serial_number}')
 
@@ -14,16 +13,14 @@ class Label:
         resize = qr.resize((150,150))
         logo = Image.open('./assets/logo.png')
 
-        label_template = Image.new("RGBA", (406, 203), (0, 0, 0, 0))
+        self.label_template = Image.new("RGB", (406, 203), "white")
 
-        label_template.paste(resize, (0, 0))
-        label_template.paste(logo, (150, 50))
+        self.label_template.paste(resize, (0, 0))
+        self.label_template.paste(logo, (150, 50))
 
         font = ImageFont.truetype("./fonts/Roboto-Regular.ttf", 25)
 
-        draw = ImageDraw.Draw(label_template)
+        draw = ImageDraw.Draw(self.label_template)
         draw.text((0, 170), f"S/N - {self.serial_number}", font=font, fill="black")
-
-        label_template.save("label.png")
-        self.zpl = ZebrafyImage(label_template).to_zpl()
-        return self.zpl
+        
+        self.zebra_image = ZebrafyImage(self.label_template, invert=True).to_zpl()
