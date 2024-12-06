cf46f6199ce466fe874d46e6a709a2fd2e2e8726
Shawn Weed <127861884+shawn-weed@users.noreply.github.com>
2024-12-06 14:13:04 -0500

    Changed to filter by device type

--- app.py
+++ app.py
@@ -8,14 +8,12 @@ load_dotenv()
 
 z = Zebra('ZDesigner ZD411-203dpi ZPL')
 
-hr_file = os.environ.get("HOMEROOM_FILE")
 device_file = os.environ.get("DEVICE_FILE")
 
-hr = pd.read_excel(hr_file, usecols='C')
-devices = pd.read_csv(device_file, usecols=[2,7])
+devices = pd.read_csv(device_file)
 
-df = pd.merge(left = devices, right = hr, how='right', left_on='Student (email)', right_on='Email')
-
-for index, row in df.iterrows():
-    label = Label(f"{row['Serial Number']}")
-    z.output(label.zebra_image)
+for index, row in devices.iterrows():
+    if f"{row['model']}" == 'HP Chromebook 11 G9 EE':
+        print(f"{row['serialNumber']}")
+        label = Label(f"{row['serialNumber']}")
+        z.output(label.zebra_image)
